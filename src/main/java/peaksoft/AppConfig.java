48c51d50246316c021a290ed02e5333109509824
User <trrioiw>
2022-02-10 19:30:37 +0600

    test

--- src/main/java/peaksoft/AppConfig.java
+++ src/main/java/peaksoft/AppConfig.java
@@ -1,14 +1,22 @@
 package peaksoft;
 
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.*;
 
 @Configuration
+@ComponentScan("peaksoft")
 public class AppConfig {
     @Bean(name="helloworld")
     public HelloWorld getHelloWorld() {
         HelloWorld helloWorld = new HelloWorld();
         helloWorld.setMessage("Hello World!");
         return helloWorld;
+
+
+    }
+    @Bean(name = "cat")
+    @Scope("prototype")
+    public Cat cat(){
+        Cat cat=new Cat("Rahim",19);
+        return cat;
     }
 }
